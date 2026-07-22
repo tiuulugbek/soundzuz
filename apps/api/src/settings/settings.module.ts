bad08ec531f06573dd6313b59a0dc3cc049fef09
tiuulugbek <tiuulugbek@gmail.com>
2026-07-22 09:29:31 +0500

    Expose public branch and service APIs

--- apps/api/src/settings/settings.module.ts
+++ apps/api/src/settings/settings.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module.js";
 import { AdminSettingsController } from "./admin-settings.controller.js";
+import { PublicSettingsController } from "./public-settings.controller.js";
 import { SettingsService } from "./settings.service.js";
 
-@Module({ imports: [AuthModule], controllers: [AdminSettingsController], providers: [SettingsService] })
+@Module({ imports: [AuthModule], controllers: [AdminSettingsController, PublicSettingsController], providers: [SettingsService] })
 export class SettingsModule {}
