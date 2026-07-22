dfae6a958b84d3a8131355d7d8d1f75327342833
tiuulugbek <tiuulugbek@gmail.com>
2026-07-22 09:41:40 +0500

    Provide roles guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,11 +2,8 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller.js";
 import { AuthService } from "./auth.service.js";
 import { JwtAuthGuard } from "./jwt-auth.guard.js";
+import { RolesGuard } from "./roles.guard.js";
 import { RateLimitService } from "../common/rate-limit.service.js";
 
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService, JwtAuthGuard, RateLimitService],
-  exports: [JwtAuthGuard],
-})
+@Module({controllers:[AuthController],providers:[AuthService,JwtAuthGuard,RolesGuard,RateLimitService],exports:[JwtAuthGuard,RolesGuard]})
 export class AuthModule {}
