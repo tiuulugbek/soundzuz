dbe8e5e84fcaaa910fc0d7d838303b10c3b1ad20
tiuulugbek <tiuulugbek@gmail.com>
2026-07-22 09:46:52 +0500

    Import auth guards in media module

--- apps/api/src/media/media.module.ts
+++ apps/api/src/media/media.module.ts
@@ -1,10 +1,7 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
 import { MediaController } from "./media.controller.js";
 import { MediaService } from "./media.service.js";
 
-@Module({
-  controllers: [MediaController],
-  providers: [MediaService],
-  exports: [MediaService],
-})
+@Module({imports:[AuthModule],controllers:[MediaController],providers:[MediaService],exports:[MediaService]})
 export class MediaModule {}
