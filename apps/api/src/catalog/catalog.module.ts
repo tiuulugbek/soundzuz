ef4253d022e2efae31f0107a882ea57589518d81
tiuulugbek <tiuulugbek@gmail.com>
2026-07-22 09:46:42 +0500

    Import auth guards in catalog module

--- apps/api/src/catalog/catalog.module.ts
+++ apps/api/src/catalog/catalog.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
 import { AdminCatalogController } from "./admin-catalog.controller.js";
 import { CatalogController } from "./catalog.controller.js";
 import { CatalogService } from "./catalog.service.js";
 
-@Module({ controllers: [CatalogController, AdminCatalogController], providers: [CatalogService] })
+@Module({imports:[AuthModule],controllers:[CatalogController,AdminCatalogController],providers:[CatalogService]})
 export class CatalogModule {}
